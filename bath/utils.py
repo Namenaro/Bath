761cd9c077a0d0e680a2ba9206752d080df0b92e
neuro <nanenaro@gmail.com>
2017-12-22 13:35:43 +0300

    Initial commit

---
+++ bath/utils.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*
+import os
+import cv2
+import shutil
+import pickle
---
+++ bath/world.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*
+import os
+import cv2
+import random
+import numpy as np
+
+class SessionHistory:
+    def __init__(self, initial_coord):
+        self.foveals = []  # матрица  [0,1] значений
+        self.contexts = [] # матрица  [0,1] значений
+        self.initial_coord =  initial_coord # коордиината начала траектории на картинке
+        self.actions = []
+
+    def add(self, fovea_matrix, context_matrix, action):
+        self.foveals.append(fovea_matrix)
+        self.contexts.append(context_matrix)
+        self.actions.append(action)
+
+    def size(self):
+        return len(self.foveals)
+
+    def get_foveal_dataset(self):
+        dataset = np.array(self.foveals, dtype='float32')
+        flatten_len = np.prod(self.foveals[0].shape[1:]) # если матрица 3 на 3, то будет 9
+        dataset = dataset.reshape((len(dataset), flatten_len))
+        return dataset
+
+    def get_extra_foveal_dataset(self):
+        assert len(self.foveals) == len(self.actions)
+        dataset = self.get_foveal_dataset()
+        for i in range(len(self.foveals)):
+            dataset[i] = np.concatenate((dataset[i]), np.array(self.actions[i], dtype='float32'))
+        return dataset
+
+
+class World:
+    def __init__(self, fovea_side, retina_padding):
+        self.PADDING = 100 # при выборе начала новой сессии, точка выбирается случайно на такой отступе от края картинки
+        self.fovea_side = fovea_side
+        self.retina_padding = retina_padding
+        self.session_history = None
+        self.curr_picture = None
+        self.current_coord = None
+
+    def set_picture(self, path):
+        assert os.path.isfile(path)
+        self.curr_picture = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        self.curr_picture = self.curr_picture* float(1) / float(255)
+
+    def reset(self):
+        self.clear_session()
+        self._set_random_left_top()
+
+    def clear_session(self):
+        self.session_history = []
+        self.top_left_x = None
+        self.top_left_y = None
+
+    def clear_history(self):
+        self.session_history = []
+
+    def action(self, command):
+        assert self.top_left_y is not None and self.top_left_x is not None
+        self.top_left_x += command.dx
+        self.top_left_y += command.dy
+
+        matrix_fovea = self._get_subframe(side=self.fovea_side,
+                                                 x=self.top_left_x,
+                                                 y=self.top_left_y)
+        matrix_retina = self._get_subframe(side=self.fovea_side+self.retina_padding,
+                                                  x=self.top_left_x-self.retina_padding,
+                                                  y=self.top_left_y-self.retina_padding)
+
+
+        self.session_history.append(history_record)
+        cv2.rectangle(self.curr_picture,
+                      pt1=(self.top_left_x, self.top_left_y),
+                      pt2=(self.top_left_x+self.fovea_side, self.top_left_y+self.fovea_side),
+                      color=(255,0,0))
+        return result
+
+    def visualise_history(self):
+        if len(self.session_history) == 0:
+            log("my warning: no history to visualise")
+        i = 0
+        for entry in self.session_history:
+            self.saver.save_matrix_as_png(str(i), entry.feedback.matrix_fovea, scaling_factor=5)
+            self.saver.save_matrix_as_png('_'+str(i), entry.feedback.matrix_retina, scaling_factor=5)
+            i+=1
+        self.saver.save_matrix_as_png('_all_', self.curr_picture, scaling_factor=1)
+
+
+    def print_history(self):
+        if len(self.session_history) == 0:
+            log("my warning: no history to print...")
+        for entry in self.session_history:
+            entry.command.print()
+            entry.feedback.print()
+
+    def save_history(self, filename):
+        path = self.saver.save_obj(filename, self.session_history)
+        print (path)
+
+    #---------------------------------------------
+    def _get_subframe(self, side, x, y):
+        X1 = x
+        X2 = x + side
+        Y1 = y
+        Y2 = y + side
+        return self.curr_picture[X1:X2, Y1:Y2]
+
+    def _set_random_left_top(self):
+        min_x = self.PADDING
+        min_y = self.PADDING
+
+        max_x = self.curr_picture.shape[0] - self.PADDING
+        max_y = self.curr_picture.shape[1] - self.PADDING
+
+        side = self.fovea_side + self.retina_padding
+        assert max_x - min_x >= side, 'picture is too small'
+        assert max_y - min_y >= side, 'picture is too small'
+
+        self.top_left_x = random.randint(min_x, max_x)
+        self.top_left_y = random.randint(min_y, max_y)
+
+    def _is_out_of_bounds(self):
+        pic_min_x = self.top_left_x - self.retina_padding
+        pic_max_x = self.top_left_x + self.retina_padding + self.fovea_side
+        pic_min_y = self.top_left_y - self.retina_padding
+        pic_max_y = self.top_left_y + self.retina_padding + self.fovea_side
+        if pic_min_x < 0 or pic_min_y < 0:
+            return True
+        shape = self.curr_picture.shape
+        if pic_max_x > shape[0] or pic_max_y > shape[1]:
+            return True
+        return False
+
+    def get_fovea_dataset_flattened(self):
+        dataset = [entry.feedback.matrix_fovea for entry in self.session_history]
+        dataset = np.array(dataset, dtype='float32')
+        flatten_data_len = np.prod(dataset.shape[1:])
+        dataset = dataset.reshape((len(dataset), flatten_data_len))
+        return dataset
+
+
+
+if __name__ == '__main__':
+    picture_name = 'C:/forest2.jpg'
+    seq_len = 5
+    commands = []
+    for i in range(seq_len):
+        command = [2, 1]
+        commands.append(command)
+
+    world = World(fovea_side=4, retina_padding=4)
+    world.set_picture(picture_name)
+    world.run_session()
+    for command in commands:
+        world.action(command)
+    world.print_history()
+    world.visualise_history()
+    world.save_history(filename='pr1.pkl')
